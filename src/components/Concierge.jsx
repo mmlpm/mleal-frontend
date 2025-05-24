e8510825b9df9632d38534cc12f8330938a9f077
mmlpm <lealempresa@gmail.com>
2025-05-24 11:53:22 -0300

    Create Concierge.jsx

---
+++ src/components/Concierge.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Typography, TextField, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
+const api = 'https://mleal.onrender.com/api/concierge';
+
+const Concierge = () => {
+  const [descricao, setDescricao] = useState('');
+  const [dados, setDados] = useState([]);
+
+  const carregar = async () => {
+    const res = await axios.get(api);
+    setDados(res.data);
+  };
+
+  useEffect(() => {
+    carregar();
+  }, []);
+
+  const salvar = async () => {
+    await axios.post(api, { descricao });
+    setDescricao('');
+    carregar();
+  };
+
+  const excluir = async (id) => {
+    await axios.delete(`${api}/${id}`);
+    carregar();
+  };
+
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>Concierge</Typography>
+      <div style={{ marginBottom: 20 }}>
+        <TextField label="Tarefa" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+        <Button onClick={salvar} variant="contained" style={{ marginLeft: 10 }}>Salvar</Button>
+      </div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Tarefa</TableCell>
+              <TableCell>Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dados.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.descricao}</TableCell>
+                <TableCell>
+                  <Button onClick={() => excluir(item.id)} variant="outlined" color="error">Excluir</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Concierge;
