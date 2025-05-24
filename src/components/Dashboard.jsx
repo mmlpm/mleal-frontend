c988c3e6e1fc91e94bf8d35ff6ce8edf9b7d02d0
mmlpm <lealempresa@gmail.com>
2025-05-24 11:45:21 -0300

    Create Dashboard.jsx

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>Dashboard</Typography>
+      <Typography>Em desenvolvimento.</Typography>
+    </div>
+  );
+};
+
+export default Dashboard;
