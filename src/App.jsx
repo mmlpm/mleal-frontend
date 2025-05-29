d9b61c58f48ccd533a63f0fb2e4ff6c6cbfc1d41
mmlpm <lealempresa@gmail.com>
2025-05-29 19:27:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Painel from './pages/Painel';
-import Pacientes from './pages/Pacientes';
-import Planos from './pages/Planos';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Concierge from './pages/Concierge';
 import Configuracoes from './pages/Configuracoes';
+import Planos from './pages/Planos';
+import Pacientes from './pages/Pacientes';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/painel" />} />
-        <Route path="/painel" element={<Painel />} />
-        <Route path="/pacientes" element={<Pacientes />} />
-        <Route path="/planos" element={<Planos />} />
+        <Route path="/" element={<Home />} />
         <Route path="/concierge" element={<Concierge />} />
         <Route path="/configuracoes" element={<Configuracoes />} />
+        <Route path="/planos" element={<Planos />} />
+        <Route path="/pacientes" element={<Pacientes />} />
       </Routes>
     </Router>
   );
