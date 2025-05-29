70f636a66f0ab94cbe0ccdfbb37b17e35d8dd954
mmlpm <lealempresa@gmail.com>
2025-05-29 18:44:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Pacientes from "./pages/Pacientes";
-import Planos from "./pages/Planos";
-import Concierge from "./pages/Concierge";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Painel from './pages/Painel';
+import Pacientes from './pages/Pacientes';
+import Planos from './pages/Planos';
+import Concierge from './pages/Concierge';
+import Configuracoes from './pages/Configuracoes';
 
 function App() {
   return (
     <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-1 p-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/pacientes" element={<Pacientes />} />
-            <Route path="/planos" element={<Planos />} />
-            <Route path="/concierge" element={<Concierge />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/painel" />} />
+        <Route path="/painel" element={<Painel />} />
+        <Route path="/pacientes" element={<Pacientes />} />
+        <Route path="/planos" element={<Planos />} />
+        <Route path="/concierge" element={<Concierge />} />
+        <Route path="/configuracoes" element={<Configuracoes />} />
+      </Routes>
     </Router>
   );
 }
