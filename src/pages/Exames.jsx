5c2514d3c169494f036964e578b161d8acd86cf0
mmlpm <lealempresa@gmail.com>
2025-05-29 15:26:43 -0300

    Create Exames.jsx

---
+++ src/pages/Exames.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Exames = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Exames</h1>
+      <p>Em desenvolvimento.</p>
+    </div>
+  );
+};
+
+export default Exames;
