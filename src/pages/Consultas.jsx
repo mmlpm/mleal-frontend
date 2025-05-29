ac5d347d4c55c5bcd574a23f3956068c3b276eaf
mmlpm <lealempresa@gmail.com>
2025-05-29 15:17:13 -0300

    Create Consultas.jsx

---
+++ src/pages/Consultas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Consultas = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Consultas</h1>
+      <p>Em desenvolvimento.</p>
+    </div>
+  );
+};
+
+export default Consultas;
