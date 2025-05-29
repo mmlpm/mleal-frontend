5f931d7a29a3efc60f92b1e6e9f35cb53751b69b
mmlpm <lealempresa@gmail.com>
2025-05-29 15:14:05 -0300

    Create Especialidades.jsx

---
+++ src/pages/Especialidades.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Especialidades = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Especialidades</h1>
+      <p>Em desenvolvimento.</p>
+    </div>
+  );
+};
+
+export default Especialidades;
