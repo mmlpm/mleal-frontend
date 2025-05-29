7e4f4f4f132a28be2ef57828a034ab02e2b64848
mmlpm <lealempresa@gmail.com>
2025-05-29 12:55:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Dashboard from './components/Dashboard';
-import Pacientes from './components/Pacientes';
-import Planos from './components/Planos';
-import Concierge from './components/Concierge';
-import Agenda from './components/Agenda';
-import Financeiro from './components/Financeiro';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Pacientes from "./pages/Pacientes";
+import Planos from "./pages/Planos";
+import Concierge from "./pages/Concierge";
 
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <div style={{ flex: 1, padding: 20 }}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/pacientes" element={<Pacientes />} />
-          <Route path="/planos" element={<Planos />} />
-          <Route path="/concierge" element={<Concierge />} />
-          <Route path="/agenda" element={<Agenda />} />
-          <Route path="/financeiro" element={<Financeiro />} />
-        </Routes>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/pacientes" element={<Pacientes />} />
+            <Route path="/planos" element={<Planos />} />
+            <Route path="/concierge" element={<Concierge />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
