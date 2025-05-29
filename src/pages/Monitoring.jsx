0e82f78d67c3722001e9fa1e832966178c1e1b6c
mmlpm <lealempresa@gmail.com>
2025-05-29 14:23:10 -0300

    Create Monitoring.jsx

---
+++ src/pages/Monitoring.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Monitoring = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Painel de Monitoramento</h1>
+      <p>Em desenvolvimento...</p>
+    </div>
+  );
+};
+
+export default Monitoring;
